a979bdd57fbd16dff051e747a0f964041219d086
Dapin1213 <dapin@github.com>
2021-06-29 20:44:32 +0700

    mengedit sedikit

--- Foodieee/Foodieee/Home.Designer.cs
+++ Foodieee/Foodieee/Home.Designer.cs
@@ -66,10 +66,9 @@ namespace Foodieee
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBoxLemon);
             this.panel1.Controls.Add(this.labelPromo);
-            this.panel1.Location = new System.Drawing.Point(-2, -6);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Location = new System.Drawing.Point(-1, -4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(452, 286);
+            this.panel1.Size = new System.Drawing.Size(301, 186);
             this.panel1.TabIndex = 0;
             // 
             // label2
@@ -78,10 +77,9 @@ namespace Foodieee
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(339, 203);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(226, 132);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 58);
+            this.label2.Size = new System.Drawing.Size(80, 36);
             this.label2.TabIndex = 6;
             this.label2.Text = "Markobar\r\n22k";
             // 
@@ -91,10 +89,9 @@ namespace Foodieee
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(177, 203);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(118, 132);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(160, 58);
+            this.label1.Size = new System.Drawing.Size(103, 36);
             this.label1.TabIndex = 5;
             this.label1.Text = "Nasi Goreng\r\n25k";
             // 
@@ -104,10 +101,9 @@ namespace Foodieee
             this.labelLemonade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelLemonade.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelLemonade.ForeColor = System.Drawing.Color.White;
-            this.labelLemonade.Location = new System.Drawing.Point(20, 203);
-            this.labelLemonade.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLemonade.Location = new System.Drawing.Point(13, 132);
             this.labelLemonade.Name = "labelLemonade";
-            this.labelLemonade.Size = new System.Drawing.Size(136, 58);
+            this.labelLemonade.Size = new System.Drawing.Size(86, 36);
             this.labelLemonade.TabIndex = 4;
             this.labelLemonade.Text = "Lemonade\r\n9k";
             this.labelLemonade.Click += new System.EventHandler(this.labelLemonade_Click);
@@ -115,10 +111,9 @@ namespace Foodieee
             // pictureBox3
             // 
             this.pictureBox3.Image = global::Foodieee.Properties.Resources._3678904283;
-            this.pictureBox3.Location = new System.Drawing.Point(342, 75);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Location = new System.Drawing.Point(228, 49);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(134, 108);
+            this.pictureBox3.Size = new System.Drawing.Size(89, 70);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
@@ -127,10 +122,9 @@ namespace Foodieee
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Foodieee.Properties.Resources.nasi_goreng_pedas_43;
-            this.pictureBox2.Location = new System.Drawing.Point(182, 75);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Location = new System.Drawing.Point(121, 49);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(134, 108);
+            this.pictureBox2.Size = new System.Drawing.Size(89, 70);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -139,10 +133,9 @@ namespace Foodieee
             // pictureBoxLemon
             // 
             this.pictureBoxLemon.Image = global::Foodieee.Properties.Resources.lemonade;
-            this.pictureBoxLemon.Location = new System.Drawing.Point(20, 75);
-            this.pictureBoxLemon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBoxLemon.Location = new System.Drawing.Point(13, 49);
             this.pictureBoxLemon.Name = "pictureBoxLemon";
-            this.pictureBoxLemon.Size = new System.Drawing.Size(134, 108);
+            this.pictureBoxLemon.Size = new System.Drawing.Size(89, 70);
             this.pictureBoxLemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxLemon.TabIndex = 1;
             this.pictureBoxLemon.TabStop = false;
@@ -154,10 +147,9 @@ namespace Foodieee
             this.labelPromo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelPromo.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelPromo.ForeColor = System.Drawing.Color.White;
-            this.labelPromo.Location = new System.Drawing.Point(4, 20);
-            this.labelPromo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPromo.Location = new System.Drawing.Point(3, 13);
             this.labelPromo.Name = "labelPromo";
-            this.labelPromo.Size = new System.Drawing.Size(119, 40);
+            this.labelPromo.Size = new System.Drawing.Size(78, 26);
             this.labelPromo.TabIndex = 0;
             this.labelPromo.Text = "Promo";
             // 
@@ -168,10 +160,9 @@ namespace Foodieee
             this.buttonRecom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonRecom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonRecom.ForeColor = System.Drawing.Color.White;
-            this.buttonRecom.Location = new System.Drawing.Point(9, 325);
-            this.buttonRecom.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonRecom.Location = new System.Drawing.Point(6, 211);
             this.buttonRecom.Name = "buttonRecom";
-            this.buttonRecom.Size = new System.Drawing.Size(153, 45);
+            this.buttonRecom.Size = new System.Drawing.Size(102, 26);
             this.buttonRecom.TabIndex = 1;
             this.buttonRecom.Text = "Recomended";
             this.buttonRecom.UseVisualStyleBackColor = false;
@@ -185,10 +176,9 @@ namespace Foodieee
             this.buttonNear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonNear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonNear.ForeColor = System.Drawing.Color.Black;
-            this.buttonNear.Location = new System.Drawing.Point(171, 325);
-            this.buttonNear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonNear.Location = new System.Drawing.Point(114, 211);
             this.buttonNear.Name = "buttonNear";
-            this.buttonNear.Size = new System.Drawing.Size(123, 45);
+            this.buttonNear.Size = new System.Drawing.Size(82, 26);
             this.buttonNear.TabIndex = 2;
             this.buttonNear.Text = "Near You";
             this.buttonNear.UseVisualStyleBackColor = false;
@@ -205,10 +195,9 @@ namespace Foodieee
             "Termurah",
             "Termahal",
             "Terlaris"});
-            this.comboBoxFilter.Location = new System.Drawing.Point(303, 325);
-            this.comboBoxFilter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxFilter.Location = new System.Drawing.Point(202, 211);
             this.comboBoxFilter.Name = "comboBoxFilter";
-            this.comboBoxFilter.Size = new System.Drawing.Size(139, 37);
+            this.comboBoxFilter.Size = new System.Drawing.Size(94, 26);
             this.comboBoxFilter.TabIndex = 3;
             this.comboBoxFilter.Text = "Filter";
             this.comboBoxFilter.SelectedIndexChanged += new System.EventHandler(this.comboBoxFilter_SelectedIndexChanged);
@@ -219,10 +208,9 @@ namespace Foodieee
             this.labelMorning.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.labelMorning.Font = new System.Drawing.Font("Montserrat", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelMorning.ForeColor = System.Drawing.Color.Black;
-            this.labelMorning.Location = new System.Drawing.Point(3, 405);
-            this.labelMorning.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelMorning.Location = new System.Drawing.Point(2, 263);
             this.labelMorning.Name = "labelMorning";
-            this.labelMorning.Size = new System.Drawing.Size(146, 40);
+            this.labelMorning.Size = new System.Drawing.Size(95, 26);
             this.labelMorning.TabIndex = 7;
             this.labelMorning.Text = "Morning";
             // 
@@ -232,10 +220,9 @@ namespace Foodieee
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(14, 572);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(9, 372);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(133, 58);
+            this.label3.Size = new System.Drawing.Size(82, 36);
             this.label3.TabIndex = 7;
             this.label3.Text = "Jus Anggur\r\n20k";
             // 
@@ -245,10 +232,9 @@ namespace Foodieee
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(176, 572);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(117, 372);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(123, 58);
+            this.label4.Size = new System.Drawing.Size(76, 36);
             this.label4.TabIndex = 10;
             this.label4.Text = "Ice Cream\r\n23k";
             // 
@@ -258,20 +244,18 @@ namespace Foodieee
             this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(338, 572);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(225, 372);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(143, 58);
+            this.label5.Size = new System.Drawing.Size(87, 36);
             this.label5.TabIndex = 11;
             this.label5.Text = "SangPisang\r\n33k";
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::Foodieee.Properties.Resources.Sang_Pisang_Nugget_Pisang_Kekinian;
-            this.pictureBox6.Location = new System.Drawing.Point(340, 460);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox6.Location = new System.Drawing.Point(227, 299);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(134, 108);
+            this.pictureBox6.Size = new System.Drawing.Size(89, 70);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 9;
             this.pictureBox6.TabStop = false;
@@ -280,10 +264,9 @@ namespace Foodieee
             // pictureBox5
             // 
             this.pictureBox5.Image = global::Foodieee.Properties.Resources.REST_284_727x720_FIT_AND_TRIM_f1c371073b2ffe160cbf115724ff355d;
-            this.pictureBox5.Location = new System.Drawing.Point(180, 460);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox5.Location = new System.Drawing.Point(120, 299);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(134, 108);
+            this.pictureBox5.Size = new System.Drawing.Size(89, 70);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 8;
             this.pictureBox5.TabStop = false;
@@ -292,10 +275,9 @@ namespace Foodieee
             // pictureBox4
             // 
             this.pictureBox4.Image = global::Foodieee.Properties.Resources.download;
-            this.pictureBox4.Location = new System.Drawing.Point(9, 460);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox4.Location = new System.Drawing.Point(6, 299);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(134, 108);
+            this.pictureBox4.Size = new System.Drawing.Size(89, 70);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 7;
             this.pictureBox4.TabStop = false;
@@ -303,10 +285,10 @@ namespace Foodieee
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(450, 769);
+            this.ClientSize = new System.Drawing.Size(300, 500);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -319,7 +301,6 @@ namespace Foodieee
             this.Controls.Add(this.buttonRecom);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
